f92373e720e379e4088f2536942047bd2dba3aab
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-12-11 16:39:54 +0100

    update small bugs

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,124 +1,116 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-
-#if defined (__LINUX__)
-#error "[Compiling error] Rhino can't run on this OS."
+ 
+/* Check if the compiler thinks you are targeting the wrong operating system. */
+#if defined(__linux__)
+#error "You are not using a cross-compiler, you will most certainly run into trouble"
 #endif
-
+ 
+/* This tutorial will only work for the 32-bit ix86 targets. */
 #if !defined(__i386__)
-// #error "[Compiling error] The kernel needs to be compiles with a ix86 compiler"
+//#error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
-
-/** Setup the hardware text-mode colors **/
-/** Set the ansi colors for usermode as constants **/
-enum vga_colors {
-    vga_color_black = 0,
-    vga_color_blue = 1,
-    vga_color_green = 2,
-    vga_color_cyan = 3,
-    vga_color_red = 4,
-    vga_color_magenta = 5,
-    vga_color_brown = 6,
-    vga_color_grey = 7,
-    vga_color_lightgrey = 8,
-    vga_color_lightblue = 9,
-    vga_color_lightgreen = 10,
-    vga_color_lightcyan = 11,
-    vga_color_lightred = 12,
-    vga_color_lightmagenta = 13,
-    vga_color_lightbrown = 14,
-    vga_color_white = 15,
+ 
+/* Hardware text mode color constants. */
+enum vga_color {
+	VGA_COLOR_BLACK = 0,
+	VGA_COLOR_BLUE = 1,
+	VGA_COLOR_GREEN = 2,
+	VGA_COLOR_CYAN = 3,
+	VGA_COLOR_RED = 4,
+	VGA_COLOR_MAGENTA = 5,
+	VGA_COLOR_BROWN = 6,
+	VGA_COLOR_LIGHT_GREY = 7,
+	VGA_COLOR_DARK_GREY = 8,
+	VGA_COLOR_LIGHT_BLUE = 9,
+	VGA_COLOR_LIGHT_GREEN = 10,
+	VGA_COLOR_LIGHT_CYAN = 11,
+	VGA_COLOR_LIGHT_RED = 12,
+	VGA_COLOR_LIGHT_MAGENTA = 13,
+	VGA_COLOR_LIGHT_BROWN = 14,
+	VGA_COLOR_WHITE = 15,
 };
-
-static const size_t vga_width = 80;
-static const size_t vga_height = 25;
-
-static inline uint8_t vga_entry_color( enum vga_color fg, enum vga_color bg ) {
-    return fg | bg << 4;
+ 
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
+{
+	return fg | bg << 4;
 }
-
-static inline uint8_t vga_entry( unsigned char uc, uint8_t color ) {
-    return ( uint8_t ) uc | ( uint16_t ) color  << 8;
+ 
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color) 
+{
+	return (uint16_t) uc | (uint16_t) color << 8;
 }
-
-size_t strlen( const char* str ) {
-    size_t len = 0;
-    while ( str[ len ] ) {
-        len++;
-    }
-
-    return len;
+ 
+size_t strlen(const char* str) 
+{
+	size_t len = 0;
+	while (str[len])
+		len++;
+	return len;
 }
-
-static terminal_row;
-static terminal_column;
-static terminal_color;
-static terminal_buffer;
-
-void terminal_initialize ( void ) {
-    terminal_row = 0;
-    terminal_column = 0;
-    terminal_buffer = ( uint16_t* ) 0xB8000;
-    terminal_color = vga_entry_color( vga_color_lightgrey, vga_color_black );
-    for ( size_t y = 0; y < vga_height; y++ ) {
-        for ( size_t x = 0; x < vga_width; x++ ) {
-            const size_t index = y * vga_width + x;
-            terminal_buffer[ index ] = vga_entry( '  ', terminal_color );
-        }
-    }
-
-    printf("[Load the kernel] passed");
-
+ 
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+ 
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+uint16_t* terminal_buffer;
+ 
+void terminal_initialize(void) 
+{
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0xB8000;
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = vga_entry(' ', terminal_color);
+		}
+	}
 }
-
-void terminal_setcolor ( uint8_t color ) {
-    terminal_color = color;
+ 
+void terminal_setcolor(uint8_t color) 
+{
+	terminal_color = color;
 }
-
-void terminal_putentryat ( char c, uint8_t color, size_t x, size_t y ) {
-    const size_t index = y * vga_width + x;
-    terminal_buffer[ index ] = vga_entry( c, color );
+ 
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
+{
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = vga_entry(c, color);
 }
-
-void terminal_putchar ( char c ) {
-    switch ( c ) {
-        case '\n': {
-                terminal_column = 0;
-                terminal_row ++;
-                break;
-        }
-    
-        default: {
-                const size_t index = (vga_height * terminal_row) + terminal_column; // Like before, calculate the buffer index
-                terminal_buffer[index] = ((uint16_t)terminal_color << 8) | c;
-                terminal_column ++;
-                break;
-        }
-    }
-
-    terminal_putentryat( c, terminal_color, terminal_column, terminal_row );
-    if ( ++terminal_column == vga_width ) {
-        terminal_column = 0;
-        if ( ++terminal_row == vga_height ) {
-            terminal_row = 0;
-        }
-    }
+ 
+void terminal_putchar(char c) 
+{
+	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	if (++terminal_column == VGA_WIDTH) {
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT)
+			terminal_row = 0;
+	}
 }
-
-void terminal_write ( const char* data, size_t size ) {
-    for ( size_t i = 0; i < size; i++ ) {
-        terminal_putchar( data[ i ] );
-    }
+ 
+void terminal_write(const char* data, size_t size) 
+{
+	for (size_t i = 0; i < size; i++)
+		terminal_putchar(data[i]);
 }
-
-void terminal_writestring( const char* data ) {
-    terminal_write( data, strlen( data ) );
+ 
+void terminal_writestring(const char* data) 
+{
+	terminal_write(data, strlen(data));
 }
+ 
+void kernel_main(void) 
+{
+	/* Initialize terminal interface */
+	terminal_initialize();
+ 
+	/* Newline support is left as an exercise. */
+	terminal_writestring("Rhino says Hello from Kernel!\n");
+	terminal_writestring("Developed by Timo Sarkar!\n");
 
-void kernel_main( void ) {
-    terminal_initialize( );
-    terminal_writestring( "Rhino says hello from Kernel!\n\nThis operating system is beeing mantained by Timo Sarkar 2020" );
 }
